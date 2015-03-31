570cebb163b095cfbbb283b432ddaf28b87ef0f5
Andrey Arkhipov <wibox@ya.ru>
2015-03-31 21:36:06 +0300

    Current version 31.03.2015

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+var uglify = require('gulp-uglify');
+var notify = require('gulp-notify');
+var concat = require('gulp-concat');
+var globby = require('globby');
+var clean = require('gulp-clean');
+
+function errorLog(err){
+    gulp.src("./source")
+        .pipe(notify(err));
+}
+
+var source = {
+    path: './source/',
+    html: './source/*.html',
+    css: './source/css/',
+    less: ['./source/less/build.less','./source/blocks/**/*.less'],
+    library: './source/js/*.js',
+    js: './source/blocks/*.js',
+    images: './source/img/*',
+    wless: 'source/**/*.less',
+    wjs: 'source/**/*.js'
+
+};
+
+var build = {
+    path: './public_html/',
+    css: './public_html/css',
+    js: './public_html/js',
+    images: './public_html/img'
+};
+
+gulp.task('html', function() {
+    gulp.src(source.html)
+        .pipe(gulp.dest(build.path))
+});
+
+gulp.task('less', function() {
+    gulp.src(source.less)
+        .pipe(concat('styles.less'))
+        .pipe(less().on('error', function(err){
+            errorLog(err);
+        }))
+        .pipe(gulp.dest(source.css))
+        .pipe(gulp.dest(build.css))
+});
+
+gulp.task('js', function() {
+    gulp.src(source.library)
+        .pipe(uglify())
+        .pipe(gulp.dest(build.js))
+    gulp.src(source.js)
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        //.pipe(gulp.dest('./source/js/'))
+        .pipe(gulp.dest(build.js))
+});
+
+gulp.task('clean', function() {
+    gulp.src(build.images)
+        .pipe(clean())
+});
+
+gulp.task('images', ['clean'], function() {
+    gulp.src(source.images)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest(build.images))
+});
+
+gulp.task('default', ['html', 'less', 'js', 'images'], function() {
+    gulp.src(source.path)
+        .pipe(notify("Running watch"));
+    gulp.watch(source.html, ['html']);
+    gulp.watch(source.wless, ['less']);
+    gulp.watch(source.wjs, ['js']);
+    gulp.watch(source.images, ['images']);
+});
---
+++ source/js/app.js
@@ -0,0 +1,129 @@
+$(document).ready(function() {
+    TweenLite.to(needle, 2, {rotation:-31,  transformOrigin:"bottom right"});
+    var speed = 0,
+        gearbox = 0,
+        time = 0,
+        distance = 0;
+    var startRace = function() {
+        start_timer();
+        setInterval(
+            function () {
+                console.log("time", time);
+
+                speedMS = speed*0.277777777777778;
+                console.log("speed", speedMS);
+                distance = distance + speedMS/100 ;
+                console.log("distance", distance);
+                if(distance > 400) {
+                    console.log("Гонка завершена!!!!!!!, ваше время: " + time);
+                    stop_timer();
+                }
+            },
+            10
+        );
+    };
+    var speedUpdate = function() {
+        var	kmNum = parseInt(speed) * 0.62137;
+        //make sure kmNum is a number then output
+        if ( (speed <= 195) && !isNaN(kmNum) ){
+            var speedKm = kmNum * 2 - 31;
+            $('#numbers').css('text-align', 'center');
+            $('#miles').val(kmNum.toFixed(2));
+            $('#numbers').html(kmNum.toFixed(0));
+            $('#mi-km').html('Miles');
+        } else if (!isNaN(kmNum)){
+            var speedKm= 215;
+            $('#numbers').css('text-align', 'right');
+            $('#miles').val(kmNum.toFixed(2));
+            $('#numbers').html(kmNum.toFixed(0));
+            $('#mi-km').html('Miles');
+        } else {
+            $('#miles').val('');
+            $('#kilometers').val('');
+            $('#numbers').html('');
+            $("#errmsg").html("Numbers Only").show().fadeOut(1600);
+        }
+
+        var needle = $("#needle");
+        TweenLite.to(needle, 2, {rotation:speedKm,  transformOrigin:"bottom right"});
+    };
+
+    var gearboxUpdate = function() {
+        $('#gearbox b').html(gearbox);
+    };
+    console.log(gearbox);
+    gearboxUpdate();
+
+    $(document).keyup(function(event){
+        if (event.keyCode == 38) {
+            var speedAuto = 1;
+            startAuto(speedAuto);
+            speed = speed+30;
+            speedUpdate();
+            gearbox = gearbox + 1;
+            gearboxUpdate();
+        }
+    });
+
+    $(document).keyup(function(event){
+        if (event.keyCode == 40) {
+            var speedAuto = -1;
+            startAuto(speedAuto);
+            speed = speed-30;
+            speedUpdate();
+            gearbox = gearbox - 1;
+            gearboxUpdate();
+        }
+    });
+
+    $("#start").click(function(){
+        startRace();
+
+    });
+    $("#stop").click(function(){
+        var speedAuto = -15;
+        startAuto(speedAuto);
+        $(".b-peugeot").animate({"margin-left": "+=150px"}, 100);
+        stop_timer();
+    });
+
+
+
+    var sdegree = 1;
+
+    var startAuto = function(speed) {
+        setInterval(function() {
+            sdegree = sdegree - speed;
+            //console.log("sdegree", sdegree);
+            //console.log("speed", speed);
+            var srotate = "rotate(" + sdegree + "deg)";
+
+            $(".b-peugeot__wheel").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
+            $(".b-content").css({"background-position-x" : -sdegree/2});
+
+        }, 0 );
+    }
+
+    var timer;
+    function start_timer()
+    {
+
+        if (timer) clearInterval(timer);
+        secs = 0;
+        document.getElementById('timer').innerHTML = 'Время: '+ secs + ' сек.';
+        timer = setInterval(
+            function () {
+                secs++;
+                document.getElementById('timer').innerHTML = 'Время: '+ secs/100 + ' сек.';
+                time = secs/100;
+            },
+            10
+        );
+    }
+    function stop_timer()
+    {
+        if (timer) clearInterval(timer);
+    }
+
+
+});
